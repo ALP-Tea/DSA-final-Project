f449cd2700b03a4310690d56e50ea73cb6536392
ALP-Tea <rocky40703@gmail.com>
2025-05-27 22:56:12 +0800

    Update For upload Gameplayer.cpp

--- For upload Gameplayer.cpp
+++ For upload Gameplayer.cpp
@@ -5,21 +5,23 @@ public:
     {
         if(not_initialize)
         {
-            Establish_map(info);
-            ActionDetermined();
-            not_initialize = false ;
+            std::string divisor_string = info.GetLevelInfo();
+            divisor = std::stoi(divisor_string.substr(1,divisor_string.size()-2));//得到mod用的數字
+            Establish_map(info);//掃圖建立簡易
+            ActionDetermined();//未完成，後續行動決定，預計在裡面呼叫EnqueueAction
+            not_initialize = false ;//後面直接返回
         }
         if (actions_.empty()) {
             return {PlayerActionType::None, {0, 0}};
         }
-
         const PlayerAction action = actions_.front();
         actions_.pop();
         return action;
     }
     void EnqueueAction(const PlayerAction action) { actions_.push(action); }
 private:
-    void Establish_map(const IGameInfo& info) {
+    void Establish_map(const IGameInfo& info) 
+    {
         int height = GameManagerConfig::kBoardHeight;
         int width = GameManagerConfig::kBoardWidth;
 
@@ -41,7 +43,7 @@ private:
                 }
                 CellPosition pos{r, c};
                 const auto& cell = info.GetLayeredCell(pos); 
-                // 判斷前景牆壁
+                // 判斷牆壁
                 auto foreground_ptr = cell.GetForeground().get();
                 if (foreground_ptr != nullptr &&
                     dynamic_cast<const WallCell*>(foreground_ptr) != nullptr) {
@@ -64,4 +66,6 @@ private:
     bool initial = true;
     std::vector<std::vector<int>>
             myboard;
+    int divisor ;
 };
+
