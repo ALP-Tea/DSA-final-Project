a3e8ac3e4973cb6d122ec765658be4049ef7ba5e
ALP-Tea <rocky40703@gmail.com>
2025-05-28 17:39:46 +0800

    Update For upload Gameplayer.cpp

--- For upload Gameplayer.cpp
+++ For upload Gameplayer.cpp
@@ -1,3 +1,4 @@
+#include<map>
 class GamePlayer final : public Feis::IGamePlayer
 {
 public:
@@ -8,7 +9,7 @@ public:
             std::string divisor_string = info.GetLevelInfo();
             divisor = std::stoi(divisor_string.substr(1,divisor_string.size()-2));//得到mod用的數字
             Establish_map(info);//掃圖建立簡易
-            ActionDetermined();//未完成，後續行動決定，預計在裡面呼叫EnqueueAction
+            ActionDetermined(info);//未完成，後續行動決定，預計在裡面呼叫EnqueueAction
             not_initialize = false ;//後面直接返回
         }
         if (actions_.empty()) {
@@ -24,8 +25,7 @@ private:
     {
         int height = GameManagerConfig::kBoardHeight;
         int width = GameManagerConfig::kBoardWidth;
-
-        myboard.resize(height, std::vector<int>(width, 0)); // 全部先設為0
+        myboard.resize(height, std::vector<int>(width, -9)); // 全部先設為-9
         int centerTop = GameManager::CollectionCenterConfig::kTop;
         int centerLeft = GameManager::CollectionCenterConfig::kLeft;
         int centerSize = GameManagerConfig::kGoalSize;
@@ -52,20 +52,49 @@ private:
                 }
                 // 數字礦石判斷
                 auto background_ptr = cell.GetBackground().get();
-                if (background_ptr != nullptr) {
-                    if (auto num = dynamic_cast<const NumberCell*>(background_ptr)) {
-                        myboard[r][c] = num->GetNumber();
+                if (background_ptr != nullptr) 
+                {
+                    if (auto num = dynamic_cast<const NumberCell*>(background_ptr)) 
+                    {
+                        int value = num->GetNumber()%divisor;
+                        myboard[r][c] = -5 ;//數字標-5，表示礦
+                        if(!value)
+                        {
+                            usable_mine.push_back({r,c});
+                        }
+                        else
+                        {
+                            bad_mine[value] = {r,c};
+                        }
                     }
                 }
             }
         }
     }
-    void ActionDetermined(){}
+    void BuildMiningPath(const IGameInfo& info, CellPosition src, CellPosition dst) {
+
+    }
+    void ActionDetermined(const IGameInfo& info)
+    {
+        for (auto& mine : usable_mine) {
+            BuildMiningPath(info, mine, factoryCenter); // 3. 為能整除的礦建構傳送帶
+        }
+        handle_bad_mine(info, bad_mine); // 4. 將不能整除的礦合成
+    }
+    void handle_bad_mine(const IGameInfo& info , std::map<int , CellPosition>& bad_mine)
+    {
+        
+    }
     bool not_initialize = true ;
     std::queue<PlayerAction> actions_;
     bool initial = true;
     std::vector<std::vector<int>>
             myboard;
     int divisor ;
+    CellPosition factoryCenter{
+        GameManager::CollectionCenterConfig::kTop + GameManagerConfig::kGoalSize / 2,
+        GameManager::CollectionCenterConfig::kLeft + GameManagerConfig::kGoalSize / 2
+    };
+    std::vector<CellPosition> usable_mine ;
+    std::map<int , CellPosition> bad_mine ; 
 };
-
